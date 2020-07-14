1176b6ca9dd10f2c0f5a8d6bdd236816a6674d41
Yael Raifer <raifer.dev@gmail.com>
2020-07-14 18:42:46 +0300

    added sidebar buttons

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import Styles from './Sidebar.module.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import routes from "../../routes";
 
-const Sidebar = ( props ) => {
-  return(
-    <div className={Styles.Sidebar}>
-      <div className={Styles.LogoContainer}></div>
+import styles from "./Sidebar.module.css";
+
+const Sidebar = (props) => {
+  return (
+    <div className={styles.Sidebar}>
+      <div className={styles.LogoContainer}></div>
+      <div className={styles.Menu}>
+        {routes.map((route) => (
+          <Link to={route.path} activeClassName="active">
+            <button className={styles.MenuButton}>
+              <i className={route.icon} />
+              <p>{route.title}</p>
+            </button>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,30 @@ const routes = [
     icon: "pe-7s-graph",
     component: Dashboard,
   },
+  {
+    path: "/app/balances",
+    title: "Balances",
+    icon: "pe-7s-graph",
+    component: Dashboard,
+  },
+  {
+    path: "/app/ccards",
+    title: "Credit Cards",
+    icon: "pe-7s-graph",
+    component: Dashboard,
+  },
+  {
+    path: "/app/loans",
+    title: "Loans",
+    icon: "pe-7s-graph",
+    component: Dashboard,
+  },
+  {
+    path: "/app/trades",
+    title: "Trades",
+    icon: "pe-7s-graph",
+    component: Dashboard,
+  },
 ];
 
 export default routes;
