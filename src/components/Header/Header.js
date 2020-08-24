f6d6ce3148071c1fa0e5a3989568bdd764b8ebd1
Yael Raifer <raifer.dev@gmail.com>
2020-08-24 14:28:33 +0300

    header content

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,9 @@
 import React, { useCallback, useContext } from "react";
 import { withRouter } from "react-router";
+import { makeStyles } from "@material-ui/core/styles";
 import { AppBar, Toolbar } from "@material-ui/core";
 
-import { Navbar, NavDropdown } from "react-bootstrap";
+import { NavDropdown } from "react-bootstrap";
 import { AiOutlineClockCircle } from "react-icons/ai";
 
 import firebase from "firebase.js";
@@ -13,6 +14,20 @@ import SelectBox from "components/SelectBox/SelectBox";
 import styles from "./Header.module.css";
 
 const Header = ({ history, title, setDataTimeFrame }) => {
+  const classes = makeStyles({
+    appBar: {
+      minHeight: "60px",
+      // backgroundColor: "#FBFBFB",
+    },
+    Toolbar: {
+      minHeight: "60px",
+      borderBottom: "1px solid #E5E5E5",
+    },
+    grow: {
+      flexGrow: 1,
+    },
+  })();
+
   const logtout = useCallback(
     async (event) => {
       event.preventDefault();
@@ -30,41 +45,35 @@ const Header = ({ history, title, setDataTimeFrame }) => {
   const { toggleSidebar } = useContext(LayoutContext);
 
   return (
-    <AppBar position="sticky" elevation={0} className={styles.Header}>
-      <Toolbar>
-        <div className={styles.LogoContainer}></div>
-        sdfgßß
+    <AppBar position="sticky" elevation={0} className={classes.appBar}>
+      <Toolbar className={classes.Toolbar}>
+        <div className={styles.Title} onClick={toggleSidebar}>
+          {title}
+        </div>
+        <SelectBox
+          className={styles.SelectBox}
+          onSelect={setDataTimeFrame}
+          icon={AiOutlineClockCircle}
+        >
+          <option>24 hours</option>
+          <option>past week</option>
+          <option>past 30 days</option>
+        </SelectBox>
+        <div className={classes.grow}></div>
+        <div>{currentUser.email}</div>
+        <NavDropdown title="Account" className={styles.DropDown}>
+          <NavDropdown.Item selected="selected">
+            Manage bank accounts
+          </NavDropdown.Item>
+          <NavDropdown.Item>Transfer money</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item>Settings</NavDropdown.Item>
+        </NavDropdown>
+        <button className={styles.HeaderButton} onClick={logtout}>
+          Log out
+        </button>
       </Toolbar>
     </AppBar>
-
-    // <Navbar fluid className={styles.Header}>
-    //   <div className={styles.LeftSide}>
-    //     <div className={styles.Title} onClick={toggleSidebar}>{title}</div>
-    //     <SelectBox
-    //       className={styles.SelectBox}
-    //       onSelect={setDataTimeFrame}
-    //       icon={AiOutlineClockCircle}
-    //     >
-    //       <option>24 hours</option>
-    //       <option>past week</option>
-    //       <option>past 30 days</option>
-    //     </SelectBox>
-    //   </div>
-    //   <div className={styles.RightSide}>
-    //     <div>{currentUser.email}</div>
-    //     <NavDropdown title="Account" className={styles.DropDown}>
-    //       <NavDropdown.Item selected="selected">
-    //         Manage bank accounts
-    //       </NavDropdown.Item>
-    //       <NavDropdown.Item>Transfer money</NavDropdown.Item>
-    //       <NavDropdown.Divider />
-    //       <NavDropdown.Item>Settings</NavDropdown.Item>
-    //     </NavDropdown>
-    //     <button className={styles.HeaderButton} onClick={logtout}>
-    //       Log out
-    //     </button>
-    //   </div>
-    // </Navbar>
   );
 };
 
