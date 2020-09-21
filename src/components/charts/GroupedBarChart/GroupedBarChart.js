0e573336a2eb97937f4e028fbbabceb98b0b608c
Yael Raifer <raifer.dev@gmail.com>
2020-09-21 21:20:36 +0300

    added bars data

--- src/components/charts/GroupedBarChart/GroupedBarChart.js
+++ src/components/charts/GroupedBarChart/GroupedBarChart.js
@@ -2,7 +2,7 @@ import React from "react";
 import { ResponsiveBar } from "@nivo/bar";
 import styles from "./GroupedBarChart.module.css";
 
-const GroupedBarChart = ({ data, height }) => {
+const GroupedBarChart = ({ data, bars, indexName, height }) => {
   const dynamicStyle = {
     height: height
   }
@@ -11,9 +11,9 @@ const GroupedBarChart = ({ data, height }) => {
     <div className={styles.ChartWrapper} style={dynamicStyle}>
       <ResponsiveBar
         data={data}
-        keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
-        indexBy="country"
-        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+        keys={bars}
+        indexBy={indexName}
+        margin={{ top: 20, right: 20, bottom: 30, left: 40 }}
         padding={0.15}
         innerPadding={1}
         groupMode="grouped"
@@ -56,50 +56,16 @@ const GroupedBarChart = ({ data, height }) => {
         borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
         axisTop={null}
         axisRight={null}
-        axisBottom={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "country",
-          legendPosition: "middle",
-          legendOffset: 32,
-        }}
         axisLeft={{
           tickSize: 5,
           tickPadding: 5,
           tickRotation: 0,
-          legend: "food",
           legendPosition: "middle",
           legendOffset: -40,
         }}
         labelSkipWidth={3}
         labelSkipHeight={14}
         labelTextColor={{ from: "color", modifiers: [["opacity", "1"]] }}
-        legends={[
-          {
-            dataFrom: "keys",
-            anchor: "bottom-right",
-            direction: "column",
-            justify: false,
-            translateX: 120,
-            translateY: 0,
-            itemsSpacing: 2,
-            itemWidth: 100,
-            itemHeight: 20,
-            itemDirection: "left-to-right",
-            itemOpacity: 0.85,
-            symbolSize: 20,
-            effects: [
-              {
-                on: "hover",
-                style: {
-                  itemOpacity: 1,
-                },
-              },
-            ],
-          },
-        ]}
-        tooltip={function () {}}
         animate={true}
         motionStiffness={50}
         motionDamping={8}
--- src/pages/Dashboard/Expenses/ExpensesBarChart/ExpensesBarChart.js
+++ src/pages/Dashboard/Expenses/ExpensesBarChart/ExpensesBarChart.js
@@ -1,13 +1,13 @@
 import React from "react";
 import WithBox from "HOC/withBox";
 
-import data from "./mockdata";
+import { data, bars } from "./mockdata";
 import GroupedBarChart from "components/charts/GroupedBarChart/GroupedBarChart";
 
 const ExpensesBarChart = (props) => {
   return (
     <WithBox>
-      <GroupedBarChart data={data} height={300} />
+      <GroupedBarChart data={data} bars={bars} indexName="time" height={300} />
     </WithBox>
   );
 };
