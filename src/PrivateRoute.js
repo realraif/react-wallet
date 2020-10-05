f5ac59d74ea7459cc0b128f33948973dac5f0faa
Yael Raifer <raifer.dev@gmail.com>
2020-10-05 15:16:35 +0300

    fixed reload route bug

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -3,20 +3,24 @@ import { Route, Redirect } from "react-router-dom";
 import { UserContext } from "./context/UserContext";
 
 const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
-  const {currentUser} = useContext(UserContext);
+  const { currentUser, isPending } = useContext(UserContext);
+
+  const renderRoute = (routeProps) => {
+    return !!currentUser ? (
+      <RouteComponent {...routeProps} />
+    ) : (
+      <Redirect to={"/login"} />
+    );
+  };
+
   return (
     <Route
       {...rest}
-      render={routeProps =>
-        !!currentUser ? (
-          <RouteComponent {...routeProps} />
-        ) : (
-          <Redirect to={"/login"} />
-        )
+      render={(routeProps) =>
+        isPending ? "loading..." : renderRoute(routeProps)
       }
     />
   );
 };
 
-
-export default PrivateRoute
+export default PrivateRoute;
