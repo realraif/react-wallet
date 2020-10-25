179198954557cae0880d89d4739bd5b39f4a9ceb
Yael Raifer <raifer.dev@gmail.com>
2020-10-25 16:40:41 +0200

    calc sice percentage

--- src/components/charts/NestedDonutChart/NestedDonutOptions.js
+++ src/components/charts/NestedDonutChart/NestedDonutOptions.js
@@ -42,7 +42,12 @@ const getGenericOptions = () => ({
         },
       ],
       dataLabels: {
-        enabled: false,
+        formatter: function () {
+          const point = this.point;
+          if (!point.parent) {
+            return point.name;
+          }
+        },
       },
       point: {
         events: {},
@@ -57,7 +62,6 @@ const getGenericOptions = () => ({
 
 const customiseOptions = (options, data, styles) => {
   const seriesData = getSeriesData(data, styles.colors);
-
   options.series = [{ data: seriesData, allowDrillToNode: true }];
 
   if (styles.borderColor) {
@@ -71,9 +75,25 @@ const setEvents = (options, callBackMethods) => {
   options.plotOptions.series.point.events.select = function (event) {
     setTimeout(() => {
       const selectedSlices = this.series.chart.getSelectedPoints();
-      callBackMethods.sliceClicked(selectedSlices);
+      const slicesData = selectedSlices.map((slice) => getSliceData(slice));
+      callBackMethods.sliceClicked(slicesData);
     });
   };
+};
+
+const getSliceData = ({ value, parent, name, id, series }) => {
+  const chartTotal =
+    series.__myTotal ||
+    (series.__myTotal = series.data
+      .map((p) => p.options.value || 0)
+      .reduce((a, b) => a + b));
+
+  const percentage = ((value / chartTotal) * 100).toFixed(1);
+  const group = parent && series.chart.get(parent);
+  const total = group ? group.value : chartTotal;
+  let percentageFromParent = ((value / total) * 100).toFixed(1);
+
+  return { value, parent, name, id, percentage, percentageFromParent };
 };
 
 const getSeriesData = (data, colors) => {
