3aebbb025863c23e8fd75f71871e96b1e7bdcdb6
Yael Raifer <raifer.dev@gmail.com>
2020-10-29 21:42:58 +0200

    bug fix

--- src/pages/mockdata/dashboardBalancesMock.js
+++ src/pages/mockdata/dashboardBalancesMock.js
@@ -3,9 +3,10 @@ import balancesData from "./balancesMock";
 export default () => {
   return balancesData.map(({ cc, ...balance }) => {
     const allStates = cc.reduce((arr, { states }) => arr.concat(states), []);
-    cc.forEach((card) => {
-      card.parentId = balance.id;
-    });
-    return { ...balance, cc, states: [...new Set(allStates)] };
+    const creditCards = cc.map((card) => ({
+      ...card,
+      parentId: balance.id,
+    }));
+    return { ...balance, cc: creditCards, states: [...new Set(allStates)] };
   });
 };
