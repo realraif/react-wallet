c7e8d1642ef6c26cb575ec59dcdeaea97ca56057
Yael Raifer <raifer.dev@gmail.com>
2022-09-28 15:40:01 -0500

    default user mode

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -2,16 +2,22 @@ import React, { useEffect, useState } from "react";
 import firebase from "firebase.js";
 
 export const UserContext = React.createContext();
+const defaultUser = { name: "", email: "" };
 
-const UserProvider = ({ children }) => {
+const UserProvider = ({ children, isDefaultUserMode }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
-      setCurrentUser(user);
+    if (isDefaultUserMode) {
+      setCurrentUser(defaultUser);
       setIsPending(false);
-    });
+    } else {
+      firebase.auth().onAuthStateChanged((user) => {
+        setCurrentUser(user);
+        setIsPending(false);
+      });
+    }
   }, []);
 
   return (
@@ -19,6 +25,7 @@ const UserProvider = ({ children }) => {
       value={{
         currentUser,
         isPending,
+        isDefaultUserMode
       }}
     >
       {children}
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,8 +1,13 @@
 import React, { useCallback } from "react";
 import { withRouter } from "react-router";
+import { NavLink } from "react-router-dom";
 import firebase from "firebase";
 
+import useStyles from "components/Layout/styles";
+
 const SignUp = ({ history }) => {
+  const classes = useStyles();
+
   const handleSignUp = useCallback(async event => {
     event.preventDefault();
     const { email, password } = event.target.elements;
@@ -19,16 +24,23 @@ const SignUp = ({ history }) => {
     <div>
       <h1>Sign up</h1>
       <form onSubmit={handleSignUp}>
-        <label>
-          Email
-          <input name="email" type="email" placeholder="Email" />
-        </label>
-        <label>
+        <div>
+          <label>
+            Email
+            <input name="email" type="email" placeholder="Email" />
+          </label>
+        </div>
+        <div>
+          <label>
           Password
           <input name="password" type="password" placeholder="Password" />
-        </label>
-        <button type="submit">Sign Up</button>
+          </label>
+        </div>
+        <button type="submit" className={classes.loginButton}>Sign Up</button>
       </form>
+      <NavLink to="/login" className={classes.loginNav}>
+        Login
+      </NavLink>
     </div>
   );
 };
